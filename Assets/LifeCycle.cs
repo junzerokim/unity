de83ff7697852c69de65e306965c37ded6dc5aa0
jykim2 <jykim2@xlsoft.co.kr>
2023-07-12 16:42:25 +0900

    Unity Study

---
+++ Assets/LifeCycle.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeCycle : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 vec = new Vector3(
+            Input.GetAxis("Horizontal"),
+            Input.GetAxis("Vertical"), 0);
+        transform.Translate(vec);   
+    }
+}
