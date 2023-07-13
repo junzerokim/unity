249468f495de6e954a6ba7fb26074d96cdea0b88
jykim2 <jykim2@xlsoft.co.kr>
2023-07-13 10:19:56 +0900

    Unity Study

--- Assets/LifeCycle.cs
+++ Assets/LifeCycle.cs
@@ -10,11 +10,21 @@ public class LifeCycle : MonoBehaviour
         
     }
 
+    Vector3 target = new Vector3(8, 1.5f, 0);
+    
     void Update()
     {
-        Vector3 vec = new Vector3(
-            Input.GetAxis("Horizontal"),
-            Input.GetAxis("Vertical"), 0);
-        transform.Translate(vec);   
+        // 1. MoveTowards (단순 등속 이동)
+        transform.position = Vector3.MoveTowards(transform.position, target, 1f);
+        
+        // 2. SmoothDamp (미끄러지듯이 감속 이동)
+        Vector3 velo = Vector3.zero;
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velo, 0.1f);
+        
+        // 3. Lerp (선형 보간)
+        transform.position = Vector3.Lerp(transform.position, target, 0.1f);
+        
+        // 4. SLerp (구면 선형 보간)
+        transform.position = Vector3.Slerp(transform.position, target, 0.05f);
     }
 }
