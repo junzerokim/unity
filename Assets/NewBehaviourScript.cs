a8d063d056039d285933d4344837891f263b31c8
JUNYOUNG KIM <junzerokim@gmail.com>
2023-07-21 08:13:24 +0900

    Unity Study

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    void Start()
+    void Update()
     {
-        Debug.Log("Hello Unity");         
+        if (Input.anyKeyDown)
+            Debug.Log("플레이어가 아무 키를 눌렀습니다.");
+
+        if (Input.GetMouseButtonDown(0))
+            Debug.Log("미사일 발사!");
+
+        if (Input.GetMouseButton(0))
+            Debug.Log("미사일 모으는 중...");
+
+        if (Input.GetMouseButtonUp(0))
+            Debug.Log("슈퍼 미사일 발사!!");
     }
 }
