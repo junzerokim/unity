9ecfc59d2a5f3024c457f239f96b11f354615da5
jykim2 <jykim2@xlsoft.co.kr>
2023-07-25 16:06:59 +0900

    Unity3D: Complete

---
+++ Assets/Scripts/PlayerBall.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBall : MonoBehaviour
+{
+    public float jumpPower;
+    public int itemCount;
+    public GameManagerLogic manager;
+    bool isJump;
+    Rigidbody rigid;
+    AudioSource itemSound;
+    void Awake()
+    {
+        isJump = false;
+        rigid = GetComponent<Rigidbody>();
+        itemSound = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            isJump = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Item")
+        {
+            itemCount++;
+            itemSound.Play();
+            other.gameObject.SetActive(false);
+            manager.GetItem(itemCount);
+        }
+        else if (other.gameObject.tag == "Finish")
+        {
+            if (itemCount == manager.totalItemCount)
+            {
+                if (manager.stage == 2)
+                    SceneManager.LoadScene(0);
+                else
+                    SceneManager.LoadScene(manager.stage + 1);
+            }
+            else
+            {
+                SceneManager.LoadScene(manager.stage);
+            }
+        }
+    }
+}
